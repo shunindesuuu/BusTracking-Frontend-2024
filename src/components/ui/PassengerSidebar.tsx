83620b7aa6b51356dd0b5ebff67873b09eb3b434
markasilum <markasilum13@gmail.com>
2024-08-21 20:08:08 +0800

    sidebar bus information

--- src/components/ui/PassengerSidebar.tsx
+++ src/components/ui/PassengerSidebar.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
 
 import * as Select from '@radix-ui/react-select';
 import classnames from 'classnames';
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@radix-ui/react-icons';
-
-
+import {
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpIcon,
+} from '@radix-ui/react-icons';
+import ProgressDemo from './ProgessBar';
 
 type SelectItemProps = {
   children: React.ReactNode;
@@ -17,16 +20,14 @@ const SelectItem = React.forwardRef<HTMLDivElement, SelectItemProps>(
     return (
       <Select.Item
         className={classnames(
-          'text-[13px] leading-none text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
+          'text-[13px] leading-none hover:bg-gray-50 w-full text-violet11 rounded-[3px] flex items-center h-[25px] pr-[35px] pl-[25px] relative select-none data-[disabled]:text-mauve8 data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-violet9 data-[highlighted]:text-violet1',
           className
         )}
         {...props}
         ref={forwardedRef}
       >
         <Select.ItemText>{children}</Select.ItemText>
-        <Select.ItemIndicator className="absolute left-0 w-[25px] inline-flex items-center justify-center">
-          <CheckIcon />
-        </Select.ItemIndicator>
+        
       </Select.Item>
     );
   }
@@ -35,45 +36,91 @@ const SelectItem = React.forwardRef<HTMLDivElement, SelectItemProps>(
 // export default SelectDemo;
 const PassengerSidebar = () => {
   return (
-    <div id='left-group' className='p-10 flex-col w-[400px] h-screen bg-gray-100'>
-        <div className='flex justify-start flex-col text-base top-20 relative'>
-            <p>Routes</p>
-            {/* Add more here */}
-            <Select.Root>
-    <Select.Trigger
-      className="inline-flex items-center justify-center rounded px-[15px] text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11 shadow-[0_2px_10px] shadow-black/10 hover:bg-mauve3 focus:shadow-[0_0_0_2px] focus:shadow-black data-[placeholder]:text-violet9 outline-none"
-      aria-label="Food"
+    <div
+      id="left-group"
+      className="p-5 flex-col w-[400px] h-screen bg-white top-20 relative "
     >
-      <Select.Value placeholder="Select a route" />
-      <Select.Icon className="text-violet11">
-        <ChevronDownIcon />
-      </Select.Icon>
-    </Select.Trigger>
-    <Select.Portal>
-      <Select.Content className="overflow-hidden bg-white  rounded-md shadow-[0px_10px_38px_-10px_rgba(22,_23,_24,_0.35),0px_10px_20px_-15px_rgba(22,_23,_24,_0.2)]">
-        <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
-          <ChevronUpIcon />
-        </Select.ScrollUpButton>
-        <Select.Viewport className="p-[5px]">
-          <Select.Group>
-            
-            <SelectItem value="apple">Route 1</SelectItem>
-            <SelectItem value="banana">Route 2</SelectItem>
-            <SelectItem value="blueberry">Route 3</SelectItem>
-          </Select.Group>
+            {/* <div className="flex justify-start flex-col text-base top-20 relative ">
+       
+      </div> */}
+      <p>Routes</p>
 
+       <Select.Root>
+          <Select.Trigger
+            className="inline-flex items-center justify-between rounded px-[15px]  text-[13px] leading-none h-[35px] gap-[5px] bg-white text-violet11  hover:bg-mauve3 border  data-[placeholder]:text-violet9 outline-none w-full"
+            aria-label="Food"
+          >
+            <Select.Value placeholder="All" />
+            <Select.Icon className="text-violet11">
+              <ChevronDownIcon />
+            </Select.Icon>
+          </Select.Trigger>
           
-        </Select.Viewport>
-        <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
-          <ChevronDownIcon />
-        </Select.ScrollDownButton>
-      </Select.Content>
-    </Select.Portal>
-  </Select.Root>
-            
-        </div>
-    </div>
-  )
-}
+          <Select.Portal >
+            <Select.Content
+              className="SelectContent bg-white shadow-md"
+              position="popper"
+              sideOffset={5}
+            >
+              <Select.ScrollUpButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+                <ChevronUpIcon />
+              </Select.ScrollUpButton>
 
-export default PassengerSidebar
+              <Select.Viewport className="p-[5px] rounded-sm">
+                <Select.Group>
+                <SelectItem value="all" >All</SelectItem>
+                  <SelectItem value="route1" >Route 1</SelectItem>
+                  <SelectItem value="route2">Route 2</SelectItem>
+                  <SelectItem value="route3">Route 3</SelectItem>
+                </Select.Group>
+
+              </Select.Viewport>
+              <Select.ScrollDownButton className="flex items-center justify-center h-[25px] bg-white text-violet11 cursor-default">
+                <ChevronDownIcon />
+              </Select.ScrollDownButton>
+
+            </Select.Content>
+          </Select.Portal>
+        </Select.Root>
+
+        <p className='mt-10 mb-2'>Bus Information</p>
+        <div className='bg-white h-52 w-full rounded-md border'>
+
+          <div className='bg-white border-b w-full h-1/4 flex items-center justify-between p-2 rounded-t-md'>
+            <div className='flex gap-2'>
+            <p>Bus Number:</p>
+            <p>12345</p>
+            </div>
+            <p className='underline text-green-400'>Toril Line</p>
+
+          </div>
+
+          <div className='h-3/4 flex flex-col'>
+            
+            
+            <div className='w-full h-fit flex flex-col p-2'>
+              <p className='mb-1'>Bus Capacity: 50</p>
+              <p className='text-sm '>Taken: 30</p>
+              <p className='text-sm'>Available: 20</p>
+
+
+            </div>
+            <div className='flex flex-col justify-center align-middle h-1/2 p-2'>
+              <ProgressDemo/>
+              <div className='flex text-xs justify-between'>
+                <p>ADDU</p>
+                <p>GMALL</p>
+                <p>VPLAZA</p>
+                <p>ABRZA</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+
+    </div>
+  );
+};
+
+export default PassengerSidebar;
---
+++ src/components/ui/ProgessBar.tsx
@@ -0,0 +1,31 @@
+"use client"
+import React, { useEffect, useState } from 'react';
+import * as Progress from '@radix-ui/react-progress';
+
+const ProgressDemo = () => {
+  const [progress, setProgress] = useState(13);
+  
+  useEffect(() => {
+    const timer = setTimeout(() => setProgress(40), 500);
+    return () => clearTimeout(timer);
+  }, []);
+
+  return (
+    <Progress.Root
+      className="relative overflow-hidden bg-gray-100 rounded-full w-[300px] h-[25px]"
+      style={{
+        // Fix overflow clipping in Safari
+        // https://gist.github.com/domske/b66047671c780a238b51c51ffde8d3a0
+        transform: 'translateZ(0)',
+      }}
+      value={progress}
+    >
+      <Progress.Indicator
+        className="bg-[#34C759] w-full h-full transition-transform duration-[660ms] ease-[cubic-bezier(0.65, 0, 0.35, 1)]"
+        style={{ transform: `translateX(-${100 - progress}%)` }}
+      />
+    </Progress.Root>
+  );
+};
+
+export default ProgressDemo;
