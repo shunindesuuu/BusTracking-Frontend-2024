c3c405a50f1690d5f7e9e2fc3129bb5d8cc8e90d
seanchvz <seann.chvz@gmail.com>
2024-09-03 19:52:38 +0800

    feat: create buses page with contents (not functional)

--- src/app/buses/page.tsx
+++ src/app/buses/page.tsx
@@ -4,13 +4,13 @@ const Buses: React.FC = () => {
   return (
     <div className="flex justify-center container mx-auto mt-20 p-4 gap-4">
       <Link
-        href="/view-buses"
+        href="/buses-view-buses"
         className="flex-grow h-20 bg-gray-100 flex items-center justify-center text-center rounded-md shadow-md hover:bg-gray-200 transition-all"
       >
         View Buses
       </Link>
       <Link
-        href="/assign-driver"
+        href="/buses-assign-driver"
         className="flex-grow h-20 bg-gray-100 flex items-center justify-center text-center rounded-md shadow-md hover:bg-gray-200 transition-all"
       >
         Assign Driver
